3ba49dda9583a42b162f7b883546ca59c04019ba
Aejas <Aejas@DESKTOP-P04SCBF>
2025-07-21 16:34:41 +0530

    Hey this is my Day 1 programs

---
+++ C2TC_CoreJavaProgram/src/Day1/basics/Addition.java
@@ -0,0 +1,14 @@
+package Day1.basics;
+import java.util.*;
+public class Addition {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		a=Integer.parseInt(args[0]);
+		b=Integer.parseInt(args[1]);
+		c=a+b;
+		System.out.println("addition of a="+a+"and b ="+b+"is"+c);
+
+	}
+
+}
---
+++ C2TC_CoreJavaProgram/src/Day1/entity/CustomerDemo.java
@@ -0,0 +1,11 @@
+package Day1.entity;
+
+public class CustomerDemo {
+	public static void main(String args[]) {
+Customer c1=new Customer();
+c1.setCid(101);
+c1.setCname("Raja");
+c1.setCity("Puducherry");
+System.out.println(c1);
+	}
+}
