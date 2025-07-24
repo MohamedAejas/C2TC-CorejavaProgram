f248d148e7d8ef47efa397f89856caca9edb62f5
Aejas <Aejas@DESKTOP-P04SCBF>
2025-07-24 13:48:11 +0530

    This is my day4 assignment

---
+++ C2TC_CoreJavaProgram/src/secondpackage/Executor.java
@@ -0,0 +1,10 @@
+package secondpackage;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ C2TC_CoreJavaProgram/src/thirdpackage/PrivateConstructorDemo.java
@@ -0,0 +1,10 @@
+package thirdpackage;
+
+public class PrivateConstructorDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
