3d448f63b20ec61e2a4d0ad1807e19279512dbc4
Aejas <Aejas@DESKTOP-P04SCBF>
2025-07-24 13:49:14 +0530

    This is my day4 assignment

--- C2TC_CoreJavaProgram/src/thirdpackage/PrivateConstructorDemo.java
+++ C2TC_CoreJavaProgram/src/thirdpackage/PrivateConstructorDemo.java
@@ -1,10 +1,17 @@
 package thirdpackage;
 
 public class PrivateConstructorDemo {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String s[]) {
+		// Can't create object with private constructor
+		// MyClass m1=new MyClass();
+		
+		MyClass m=MyClass.getObject();
+		m.setId(10);
+		
+		MyClass m1=MyClass.getObject();
+		
+		System.out.println(m);
+		System.out.println(m1);
+		
 	}
-
 }
