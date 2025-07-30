820855627bab208b7ce3759a167aa73db8b8540f
Aejas <Aejas@DESKTOP-P04SCBF>
2025-07-30 14:30:07 +0530

    Day5 and Day6 assignment

---
+++ C2TC_CoreJavaProgram/src/Assignment4/Indigo.java
@@ -0,0 +1,40 @@
+package Assignment4;
+
+public class Indigo implements Airfare {
+    private int hours;
+    private double costPerHour;
+
+    
+    public Indigo() {
+        this.hours = 0;
+        this.costPerHour = 0.0;
+    }
+   
+    public Indigo(int hours, double costPerHour) {
+        this.hours = hours;
+        this.costPerHour = costPerHour;
+    }
+    
+    public int getHours() {
+        return hours;
+    }
+    public double getCostPerHour() {
+        return costPerHour;
+    }
+    
+    public void setHours(int hours) {
+        this.hours = hours;
+    }
+    public void setCostPerHour(double costPerHour) {
+        this.costPerHour = costPerHour;
+    }
+   
+    @Override
+    public double calculateAmount() {
+        return hours * costPerHour * 8;
+    }
+    @Override
+    public void display() {
+        System.out.printf("%.2f\n", calculateAmount());
+    }
+}
---
+++ C2TC_CoreJavaProgram/src/Assignment4/Main.java
@@ -0,0 +1,29 @@
+package Assignment4;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice = sc.nextInt();
+        int hours = sc.nextInt();
+        double costPerHour = sc.nextDouble();
+        Airfare flight;
+        switch (choice) {
+            case 1:
+                flight = new AirIndia(hours, costPerHour);
+                break;
+            case 2:
+                flight = new Kingfisher(hours, costPerHour);
+                break;
+            case 3:
+                flight = new Indigo(hours, costPerHour);
+                break;
+            default:
+                System.out.println("Invalid choice");
+                sc.close();
+                return;
+        }
+        flight.display();
+        sc.close();
+    }
+}
+
