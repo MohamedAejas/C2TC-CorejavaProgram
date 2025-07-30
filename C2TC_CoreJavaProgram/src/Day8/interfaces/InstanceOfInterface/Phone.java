d6f44f2a704be0e91220c75a17ff557b435cc934
Aejas <Aejas@DESKTOP-P04SCBF>
2025-07-30 14:14:30 +0530

    Day8 instance marker and nested

---
+++ C2TC_CoreJavaProgram/src/Day8/interfaces/InstanceOfInterface/Phone.java
@@ -0,0 +1,7 @@
+package Day8.interfaces.InstanceOfInterface;
+
+public interface Phone {
+	void call();
+
+	void sms();
+}
---
+++ C2TC_CoreJavaProgram/src/Day8/interfaces/MarkerInterfaces/Registrable.java
@@ -0,0 +1,5 @@
+package Day8.interfaces.MarkerInterfaces;
+
+public interface Registrable {
+
+}
